8fca1011a862476c690383e195128e6859df2e23
Ahsan <ahsan.nasir2020@gmail.com>
2019-05-31 02:05:06 +0200

    mandelbrot

--- mopp-2018-t3-himeno/himeno.c
+++ mopp-2018-t3-himeno/himeno.c
@@ -31,6 +31,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/time.h>
+#include <pthread.h>
 
 #define MR(mt,n,r,c,d)  mt->m[(n) * mt->mrows * mt->mcols * mt->mdeps + (r) * mt->mcols* mt->mdeps + (c) * mt->mdeps + (d)]
 
@@ -49,24 +50,35 @@ int newMat(Matrix* Mat, int mnums, int mrows, int mcols, int mdeps);
 void clearMat(Matrix* Mat);
 void mat_set(Matrix* Mat,int l,float z);
 void mat_set_init(Matrix* Mat);
-float jacobi(int n,Matrix* M1,Matrix* M2,Matrix* M3,
-             Matrix* M4,Matrix* M5,Matrix* M6,Matrix* M7);
+float jacobi(int n);
+void* tempthread();
+void* partialJacob(void* x);
+void partJcob(void *x);
+
 
 float   omega=0.8;
 Matrix  a,b,c,p,bnd,wrk1,wrk2;
+  float  gosa,s0,ss;
+  int noOfThreads = 4;
+int step = 0;
+int    imax,jmax,kmax;
+  // pthread_t threads[noOfThreads];
 
 int
 main(int argc, char *argv[])
 {
   int    nn;
-  int    imax,jmax,kmax,mimax,mjmax,mkmax,msize[3];
-  float  gosa;
-
-  scanf("%d", &msize[0]);
-  scanf("%d", &msize[1]);
-  scanf("%d", &msize[2]);
-  scanf("%d", &nn);
+  int    mimax,mjmax,mkmax,msize[3];
   
+
+  //scanf("%d", &msize[0]);
+  //scanf("%d", &msize[1]);
+  //scanf("%d", &msize[2]);
+  //scanf("%d", &nn);
+  msize[0] = 64;
+  msize[1] = 64;
+  msize[2] = 128;
+  nn = 10;
   mimax= msize[0];
   mjmax= msize[1];
   mkmax= msize[2];
@@ -103,7 +115,7 @@ main(int argc, char *argv[])
   /*
    *    Start measuring
    */
-  gosa = jacobi(nn,&a,&b,&c,&p,&bnd,&wrk1,&wrk2);
+  jacobi(nn);
 
   printf("%.6f\n",gosa);
 
@@ -120,6 +132,154 @@ main(int argc, char *argv[])
   
   return (0);
 }
+
+
+struct JacobArgs {
+  Matrix M1;
+  Matrix M2;
+  Matrix M3;
+  Matrix M4;
+  Matrix M5;
+  Matrix M6;
+  Matrix M7;
+
+};
+
+
+typedef struct JacobArgs struct1;
+
+float
+jacobi(int nn)
+{
+  printf("came her boi");
+ int n,i,j,k;
+ 
+ struct1 *x;
+ x=(struct1 *)malloc(sizeof(struct1));
+ x->M1 = a;
+ x->M2 = b;
+ x->M3 = c;
+ x->M4 = p;
+ x->M5 = bnd;
+ x->M6 = wrk1;
+ x->M7 = wrk2;
+ 
+  printf("came her boi2");
+ //imax= p->mrows-1;
+  //jmax= p->mcols-1;
+  //kmax= p->mdeps-1;
+      	
+  //for(n=0 ; n<nn ; n++){
+    gosa = 0.0;
+ pthread_t threads[noOfThreads];
+step = 0;
+ printf("i have %d threads", noOfThreads);
+  for (int th = 0; th < noOfThreads ; th++ ) {
+      pthread_create(&threads[th], NULL, partialJacob, (void*)(x));
+  }
+for (int tj = 0; tj < noOfThreads; tj++)
+     {	pthread_join(threads[tj], NULL); }
+
+ 
+      partJcob(x);
+    
+   //} /* end n loop */
+  //printf("optimized gosa: %f", gosa);
+  return(gosa);
+}
+
+void* tempthread() {
+   printf("\n\n\ni am threaded");
+}
+void partJcob(void *x) {
+  
+Matrix* p;
+
+Matrix* wrk2;
+
+struct1 *y;
+y=(struct1*) x;
+  p = &(y->M4);
+wrk2 = &(y->M7);
+
+int i,j,k;
+     for(i=1 ; i<imax ; i++)
+      for(j=1 ; j<jmax ; j++)
+        for(k=1 ; k<kmax ; k++)
+          MR(p,0,i,j,k)= MR(wrk2,0,i,j,k);
+
+
+}
+void* partialJacob( void* x ) {
+
+Matrix* a; 
+Matrix* b;
+Matrix* c;
+Matrix* p;
+Matrix* bnd;
+Matrix* wrk1;
+Matrix* wrk2;
+
+struct1 *y;
+y=(struct1*) x;
+    a = &(y->M1);
+    b = &(y->M2);
+    c = &(y->M3);
+    p = &(y->M4);
+    bnd = &(y->M5);
+    wrk1 = &(y->M6);
+    wrk2 = &(y->M7);
+         
+  int i,j,k;
+  printf("came here");
+
+  int core = step++;
+  int ii = 0;
+   for(ii= (core * imax) / noOfThreads ; ii <= (core + 1) * imax / noOfThreads; ii++)
+  // for (i=1;i<imax;i++) 
+      {
+        i = (ii + 1);
+        printf("\ni am a threaded bouy with i: %d, my core is: %d my imax is %d my step is %d\n\n", i,core,imax,step);
+        if ( i > (imax - 1)) {
+          break;
+        }
+        for(j=1 ; j<jmax ; j++) {
+
+        for(k=1 ; k<kmax ; k++){
+    //  printf("that was some fast ass shit");
+          s0= MR(a,0,i,j,k)*MR(p,0,i+1,j,  k)
+            + MR(a,1,i,j,k)*MR(p,0,i,  j+1,k)
+            + MR(a,2,i,j,k)*MR(p,0,i,  j,  k+1)
+            + MR(b,0,i,j,k)
+             *( MR(p,0,i+1,j+1,k) - MR(p,0,i+1,j-1,k)
+              - MR(p,0,i-1,j+1,k) + MR(p,0,i-1,j-1,k) )
+            + MR(b,1,i,j,k)
+             *( MR(p,0,i,j+1,k+1) - MR(p,0,i,j-1,k+1)
+              - MR(p,0,i,j+1,k-1) + MR(p,0,i,j-1,k-1) )
+            + MR(b,2,i,j,k)
+             *( MR(p,0,i+1,j,k+1) - MR(p,0,i-1,j,k+1)
+              - MR(p,0,i+1,j,k-1) + MR(p,0,i-1,j,k-1) )
+            + MR(c,0,i,j,k) * MR(p,0,i-1,j,  k)
+            + MR(c,1,i,j,k) * MR(p,0,i,  j-1,k)
+            + MR(c,2,i,j,k) * MR(p,0,i,  j,  k-1)
+            + MR(wrk1,0,i,j,k);
+
+          ss= (s0*MR(a,3,i,j,k) - MR(p,0,i,j,k))*MR(bnd,0,i,j,k);
+
+          gosa+= ss*ss;
+          printf("\n%f", gosa);
+          MR(wrk2,0,i,j,k)= MR(p,0,i,j,k) + omega*ss;
+
+          // checktimes++;
+     //     printf("\nnexecuted %f times\n", gosa);
+        }
+      }
+      }
+
+
+
+}
+
 
 int
 newMat(Matrix* Mat, int mnums,int mrows, int mcols, int mdeps)
@@ -169,54 +329,3 @@ mat_set_init(Matrix* Mat)
         MR(Mat,0,i,j,k)= (float)(i*i)
           /(float)((Mat->mrows - 1)*(Mat->mrows - 1));
 }
-
-float
-jacobi(int nn, Matrix* a,Matrix* b,Matrix* c,
-       Matrix* p,Matrix* bnd,Matrix* wrk1,Matrix* wrk2)
-{
-  int    i,j,k,n,imax,jmax,kmax;
-  float  gosa,s0,ss;
-
-  imax= p->mrows-1;
-  jmax= p->mcols-1;
-  kmax= p->mdeps-1;
-
-  for(n=0 ; n<nn ; n++){
-    gosa = 0.0;
-
-    for(i=1 ; i<imax; i++)
-      for(j=1 ; j<jmax ; j++)
-        for(k=1 ; k<kmax ; k++){
-          s0= MR(a,0,i,j,k)*MR(p,0,i+1,j,  k)
-            + MR(a,1,i,j,k)*MR(p,0,i,  j+1,k)
-            + MR(a,2,i,j,k)*MR(p,0,i,  j,  k+1)
-            + MR(b,0,i,j,k)
-             *( MR(p,0,i+1,j+1,k) - MR(p,0,i+1,j-1,k)
-              - MR(p,0,i-1,j+1,k) + MR(p,0,i-1,j-1,k) )
-            + MR(b,1,i,j,k)
-             *( MR(p,0,i,j+1,k+1) - MR(p,0,i,j-1,k+1)
-              - MR(p,0,i,j+1,k-1) + MR(p,0,i,j-1,k-1) )
-            + MR(b,2,i,j,k)
-             *( MR(p,0,i+1,j,k+1) - MR(p,0,i-1,j,k+1)
-              - MR(p,0,i+1,j,k-1) + MR(p,0,i-1,j,k-1) )
-            + MR(c,0,i,j,k) * MR(p,0,i-1,j,  k)
-            + MR(c,1,i,j,k) * MR(p,0,i,  j-1,k)
-            + MR(c,2,i,j,k) * MR(p,0,i,  j,  k-1)
-            + MR(wrk1,0,i,j,k);
-
-          ss= (s0*MR(a,3,i,j,k) - MR(p,0,i,j,k))*MR(bnd,0,i,j,k);
-
-          gosa+= ss*ss;
-          MR(wrk2,0,i,j,k)= MR(p,0,i,j,k) + omega*ss;
-        }
-
-    for(i=1 ; i<imax ; i++)
-      for(j=1 ; j<jmax ; j++)
-        for(k=1 ; k<kmax ; k++)
-          MR(p,0,i,j,k)= MR(wrk2,0,i,j,k);
-    
-  } /* end n loop */
-
-  return(gosa);
-}
-
