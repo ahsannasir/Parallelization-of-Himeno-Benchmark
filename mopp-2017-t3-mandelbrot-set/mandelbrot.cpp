97c4826373fc3b0d7f0b17455f7209ba5209a0b2
Ahsan Nasir <ahsan.nasir2020@gmail.com>
2019-06-14 01:42:10 +0200

    //fix
    basic optimizations for sequential execution

--- mopp-2017-t3-mandelbrot-set/mandelbrot.cpp
+++ mopp-2017-t3-mandelbrot-set/mandelbrot.cpp
@@ -17,7 +17,7 @@ int main(){
 	for(int r = 0; r < max_row; ++r){
 		for(int c = 0; c < max_column; ++c){
 		z = 0; n = 0;
-			while(((z.real() * z.real()) + (z.imag() * z.imag()) < 4  && ++n < max_n)
+			while(((z.real() * z.real()) + (z.imag() * z.imag())) < 4  && ++n < max_n)
 				z = (z * z) + decltype(z)(
 					(float)c * 2 / max_column - 1.5,
 					(float)r * 2 / max_row - 1
