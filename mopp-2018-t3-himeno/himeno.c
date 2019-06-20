1f03bf59e5dc5b587bf7fa45c84e64dd71345425
Ahsan Nasir <ahsan.nasir2020@gmail.com>
2019-06-20 20:20:01 +0200

    Himeno benchmark, finalized with code cleanup

--- mopp-2018-t3-himeno/himeno.c
+++ mopp-2018-t3-himeno/himeno.c
@@ -224,10 +224,10 @@ main(int argc, char *argv[])
    // we're going to parallelize this section first.
 
   
-  //CPUs = 1;
+  
   pthread_t threads[CPUs];
   struct thread_work_t tw[CPUs];
-  //CPUs = 7;
+  
  imax= p->mrows-1;
   jmax= p->mcols-1;
   kmax= p->mdeps-1;
@@ -241,29 +241,10 @@ main(int argc, char *argv[])
     tw[i].start_n = ((imax)/CPUs)* i + 1;
     tw[i].end_n   = ((imax)/CPUs)*(i+1);
     tw[i].thread_num = i;
-    // create a thread
-   // printf("%d\n", tw[i].thread_num);
-
-   // if (i == (CPUs - 1)) {
-     //  if ( (imax - tw[i].end_n) > 0 ) {
-       //  tw[i].end_n = imax;
-      // }
-    //}
+    
     pthread_create(&threads[i], NULL, jacobi, (void*)&tw[i]);
   }
-// pthread_barrier_wait(&barrier1);
-     // Final thread for left overs
- 
-  //gosa = jacobi();
- 
-
- //   printf("main thread has start %d end %d", maintw.start_n, maintw.end_n);
-//pthread_create(&mainth, NULL, jacobi, (void*)&maintw);
-  
-//   pthread_join(mainth, NULL);
- 
-
-     struct thread_work_t maintw;
+    struct thread_work_t maintw;
     pthread_t mainth;
     maintw.start_n = (imax/CPUs) * CPUs + 1;
     maintw.end_n   = imax;
@@ -272,30 +253,18 @@ main(int argc, char *argv[])
    
   } else
    {
-     /// printf("gonna take main th");
-    // jacobi((void*)&maintw);
      pthread_t mainth;
-    // pthread_create(&mainth, NULL, jacobi, (void*)&maintw);
-    
+     pthread_create(&mainth, NULL, jacobi, (void*)&maintw);
    }
-
     for (int i=0; i<CPUs; i++) {
-        // wait for all threads
         pthread_join(threads[i], NULL);
    }
   if (maintw.start_n >= maintw.end_n) {
-   
-  
   } else
    {
-    // printf("came here 22");
-     //pthread_join(mainth, NULL);
-   
+     pthread_join(mainth, NULL);
    }
      
-    
- //   jacobi((void*)&maintw);
-//  printf("%.6f\n",gosa);
   mat_gen_output();
    pthread_mutex_destroy(&lock);
   /*
